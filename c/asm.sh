d0e88120abaa08a8ea9e34a9d2eb97f967410749
Michal Moskal <michal@moskal.me>
2020-09-21 14:35:23 +0200

    Generate .ts file with model

--- c/asm.sh
+++ c/asm.sh
@@ -2,3 +2,7 @@
 set -x
 FL="-masm-syntax-unified -mthumb -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -O3 -mfloat-abi=hard"
 arm-none-eabi-gcc -W -Wall $FL -c foo.s
+arm-none-eabi-objcopy -O binary foo.o foo.bin
+(echo "export const modelBuf = hex\`"
+node -e 'console.log(require("fs").readFileSync("foo.bin").toString("hex").replace(/.{0,128}/g, s => s + "\n"))'
+echo "\`") > foo.ts
