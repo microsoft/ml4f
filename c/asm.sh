bcf10d5f8d46412f15339397f89d155a7da255bb
Michal Moskal <michal@moskal.me>
2020-09-21 14:55:24 +0200

    Fix format

--- c/asm.sh
+++ c/asm.sh
@@ -3,6 +3,6 @@ set -x
 FL="-masm-syntax-unified -mthumb -mcpu=cortex-m4 -mfpu=fpv4-sp-d16 -O3 -mfloat-abi=hard"
 arm-none-eabi-gcc -W -Wall $FL -c foo.s
 arm-none-eabi-objcopy -O binary foo.o foo.bin
-(echo "export const modelBuf = hex\`"
+(echo "const modelBuf = hex\`"
 node -e 'console.log(require("fs").readFileSync("foo.bin").toString("hex").replace(/.{0,128}/g, s => s + "\n"))'
 echo "\`") > foo.ts
