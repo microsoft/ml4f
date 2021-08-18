fbaac25890b9a0e4718a6f9e8a448a29c719b411
Michal Moskal <michal@moskal.me>
2021-08-18 16:51:53 -0700

    Try gh-pages deploy

---
+++ pxt/build-gh-pages.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+set -x
+set -e
+rm -rf built/gh-pages
+mkdir -p built/gh-pages
+sed -e 's@src="[^"]*/@src="./@g' < pxt/index.html > built/gh-pages/index.html
+cp node_modules/@tensorflow/tfjs/dist/tf.es2017.js built/ml4f.js built/pxtml4f.js built/gh-pages/
+exit 0
