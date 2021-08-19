06d49454147e61265a157bb07840c53b768b3764
Michal Moskal <michal@moskal.me>
2021-08-18 17:04:59 -0700

    disable jekyll

--- pxt/build-gh-pages.sh
+++ pxt/build-gh-pages.sh
@@ -6,4 +6,5 @@ rm -rf built/gh-pages
 mkdir -p built/gh-pages
 sed -e 's@src="[^"]*/@src="./@g' < pxt/index.html > built/gh-pages/index.html
 cp node_modules/@tensorflow/tfjs/dist/tf.es2017.js built/ml4f.js built/pxtml4f.js built/gh-pages/
+touch built/gh-pages/.nojekyll
 exit 0
